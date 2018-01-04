e9a88eaa8a424ac5365ec53d1bbd40b717a1a131
alex <alex@DEV01>
2018-01-04 15:29:21 +0300

    add DownloadedFile class

---
+++ IO/DownloadedFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+
+namespace CoreFramework
+{
+    public class DownloadedFile
+    {
+        public string Filename { get; set; }
+        public string ContentType { get; set; }
+        public Stream Stream { get; set; }
+        public static DownloadedFile Create(string url, bool copyStream = false)
+        {
+            var df = new DownloadedFile();
+            using (WebClient web = new WebClient())
+            {
+                string hcd = web.ResponseHeaders["content-disposition"];
+                df.ContentType = web.ResponseHeaders["content-type"];
+                if (!string.IsNullOrEmpty(hcd))
+                {
+                    ContentDisposition cd = new ContentDisposition(hcd);
+                    df.Filename = cd.FileName;
+                }
+
+                var stream = web.OpenRead(url);
+                try
+                {
+                    if (copyStream)
+                    {
+                        df.Stream = new MemoryStream();
+                        stream.CopyTo(df.Stream);
+                    }
+                    else
+                    {
+                        df.Stream = stream;
+                    }
+                }
+                finally
+                {
+                    if (copyStream)
+                    {
+                        stream.Dispose();
+                    }
+                }
+            }
+
+            return df;
+        }
+    }
+}
---
+++ IO/DownloadedFileAsync.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreFramework
+{
+    public class DownloadedFileAsync<T>
+    {
+        public string Filename { get; set; }
+        public string ContentType { get; set; }
+        public Task<T> Task { get; set; }
+        public static DownloadedFileAsync<T> Create(Uri url, Func<DownloadedFileAsync<T>,Stream,T> convertResult, CancellationToken cancellationToken)
+        {
+            var df = new DownloadedFileAsync<T>();
+            using (WebClient web = new WebClient())
+            {
+                var tcs = new TaskCompletionSource<T>();
+
+                cancellationToken.Register(() => tcs.SetCanceled());
+
+                web.OpenReadCompleted += (sender, args) =>
+                {
+                    try
+                    {
+                        string hcd = web.ResponseHeaders["content-disposition"];
+                        df.ContentType = web.ResponseHeaders["content-type"];
+                        if (!string.IsNullOrEmpty(hcd))
+                        {
+                            ContentDisposition cd = new ContentDisposition(hcd);
+                            df.Filename = cd.FileName;
+                        }
+
+                        tcs.SetResult(convertResult(df, args.Result));
+                    }
+                    catch (Exception ex)
+                    {
+                        tcs.SetException(ex);
+                    }
+
+                };
+
+                web.OpenReadAsync(url);
+
+                df.Task = tcs.Task;
+            }
+
+            return df;
+        }
+
+        public static DownloadedFileAsync<Stream> Create(Uri url, CancellationToken cancellationToken)
+        {
+            return DownloadedFileAsync<Stream>.Create(url, (df, stream) => stream, cancellationToken);
+        }
+    }
+}
