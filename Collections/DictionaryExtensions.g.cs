2a7911c0dd3bfa8ffb2362fa54e94bfc46b2b836
Alexey Shirshov <interlocked@hotmail.com>
2019-09-19 11:54:23 +0300

    ConvertExtensions: add TryConvertTo methods

--- Collections/DictionaryExtensions.g.cs
+++ Collections/DictionaryExtensions.g.cs
@@ -7,7 +7,7 @@ namespace CoreFramework.CFCollections
 	public static partial class DictionaryExtensions
     {
             
-		public static int GetInt(this IDictionary<String, String> dic, string name, int def, bool throwOnParseError = false)
+		public static int GetInt(this IDictionary<String, String> dic, string name, int def, bool throwOnParseError = false, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
@@ -16,37 +16,40 @@ namespace CoreFramework.CFCollections
             int i;
             if (dic.TryGetValue(name, out s))
 			{
+				if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
 				if (throwOnParseError)
 					return int.Parse(s);
-				else if(int.TryParse(s, out i))
+				else if(int.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
 					return i;
 			}
 
             return def;
         }
 
-		public static int? GetInt(this IDictionary<String, String> dic, string name)
+		public static int? GetInt(this IDictionary<String, String> dic, string name, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
             string s;
             int i;
-            if (dic.TryGetValue(name, out s) && int.TryParse(s, out i))
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+            if (dic.TryGetValue(name, out s) && int.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
                 return i;
 
             return null;
         }
-
-		public static int GetInt(this IDictionary<String, object> dic, string name, int def = default(int))
+		
+		public static int GetInt(this IDictionary<String, object> dic, string name, int def = default(int), System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
-            object s;
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
             if (dic.TryGetValue(name, out s))
 			{
-                return (int)Convert.ChangeType(s, typeof(int));
+                return (int)Convert.ChangeType(s, typeof(int), cultureInfo);
 			}
 
             return def;
@@ -82,22 +85,23 @@ namespace CoreFramework.CFCollections
 
             return null;
         }
-
-		public static bool GetBool(this IDictionary<String, object> dic, string name, bool def = default(bool))
+		
+		public static bool GetBool(this IDictionary<String, object> dic, string name, bool def = default(bool), System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
-            object s;
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
             if (dic.TryGetValue(name, out s))
 			{
-                return (bool)Convert.ChangeType(s, typeof(bool));
+                return (bool)Convert.ChangeType(s, typeof(bool), cultureInfo);
 			}
 
             return def;
         }
             
-		public static double GetDouble(this IDictionary<String, String> dic, string name, double def, bool throwOnParseError = false)
+		public static double GetDouble(this IDictionary<String, String> dic, string name, double def, bool throwOnParseError = false, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
@@ -106,43 +110,46 @@ namespace CoreFramework.CFCollections
             double i;
             if (dic.TryGetValue(name, out s))
 			{
+				if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
 				if (throwOnParseError)
 					return double.Parse(s);
-				else if(double.TryParse(s, out i))
+				else if(double.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
 					return i;
 			}
 
             return def;
         }
 
-		public static double? GetDouble(this IDictionary<String, String> dic, string name)
+		public static double? GetDouble(this IDictionary<String, String> dic, string name, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
             string s;
             double i;
-            if (dic.TryGetValue(name, out s) && double.TryParse(s, out i))
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+            if (dic.TryGetValue(name, out s) && double.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
                 return i;
 
             return null;
         }
-
-		public static double GetDouble(this IDictionary<String, object> dic, string name, double def = default(double))
+		
+		public static double GetDouble(this IDictionary<String, object> dic, string name, double def = default(double), System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
-            object s;
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
             if (dic.TryGetValue(name, out s))
 			{
-                return (double)Convert.ChangeType(s, typeof(double));
+                return (double)Convert.ChangeType(s, typeof(double), cultureInfo);
 			}
 
             return def;
         }
             
-		public static float GetFloat(this IDictionary<String, String> dic, string name, float def, bool throwOnParseError = false)
+		public static float GetFloat(this IDictionary<String, String> dic, string name, float def, bool throwOnParseError = false, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
@@ -151,43 +158,46 @@ namespace CoreFramework.CFCollections
             float i;
             if (dic.TryGetValue(name, out s))
 			{
+				if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
 				if (throwOnParseError)
 					return float.Parse(s);
-				else if(float.TryParse(s, out i))
+				else if(float.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
 					return i;
 			}
 
             return def;
         }
 
-		public static float? GetFloat(this IDictionary<String, String> dic, string name)
+		public static float? GetFloat(this IDictionary<String, String> dic, string name, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
             string s;
             float i;
-            if (dic.TryGetValue(name, out s) && float.TryParse(s, out i))
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+            if (dic.TryGetValue(name, out s) && float.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
                 return i;
 
             return null;
         }
-
-		public static float GetFloat(this IDictionary<String, object> dic, string name, float def = default(float))
+		
+		public static float GetFloat(this IDictionary<String, object> dic, string name, float def = default(float), System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
-            object s;
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
             if (dic.TryGetValue(name, out s))
 			{
-                return (float)Convert.ChangeType(s, typeof(float));
+                return (float)Convert.ChangeType(s, typeof(float), cultureInfo);
 			}
 
             return def;
         }
             
-		public static short GetShort(this IDictionary<String, String> dic, string name, short def, bool throwOnParseError = false)
+		public static short GetShort(this IDictionary<String, String> dic, string name, short def, bool throwOnParseError = false, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
@@ -196,37 +206,88 @@ namespace CoreFramework.CFCollections
             short i;
             if (dic.TryGetValue(name, out s))
 			{
+				if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
 				if (throwOnParseError)
 					return short.Parse(s);
-				else if(short.TryParse(s, out i))
+				else if(short.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
 					return i;
 			}
 
             return def;
         }
 
-		public static short? GetShort(this IDictionary<String, String> dic, string name)
+		public static short? GetShort(this IDictionary<String, String> dic, string name, System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
             string s;
             short i;
-            if (dic.TryGetValue(name, out s) && short.TryParse(s, out i))
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+            if (dic.TryGetValue(name, out s) && short.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
                 return i;
 
             return null;
         }
-
-		public static short GetShort(this IDictionary<String, object> dic, string name, short def = default(short))
+		
+		public static short GetShort(this IDictionary<String, object> dic, string name, short def = default(short), System.Globalization.CultureInfo cultureInfo = null)
         {
             if (dic == null)
                 throw new ArgumentNullException(nameof(dic));
 
-            object s;
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
             if (dic.TryGetValue(name, out s))
 			{
-                return (short)Convert.ChangeType(s, typeof(short));
+                return (short)Convert.ChangeType(s, typeof(short), cultureInfo);
+			}
+
+            return def;
+        }
+            
+		public static decimal GetDecimal(this IDictionary<String, String> dic, string name, decimal def, bool throwOnParseError = false, System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (dic == null)
+                throw new ArgumentNullException(nameof(dic));
+
+            string s;
+            decimal i;
+            if (dic.TryGetValue(name, out s))
+			{
+				if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+				if (throwOnParseError)
+					return decimal.Parse(s);
+				else if(decimal.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
+					return i;
+			}
+
+            return def;
+        }
+
+		public static decimal? GetDecimal(this IDictionary<String, String> dic, string name, System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (dic == null)
+                throw new ArgumentNullException(nameof(dic));
+
+            string s;
+            decimal i;
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+            if (dic.TryGetValue(name, out s) && decimal.TryParse(s, System.Globalization.NumberStyles.Any, cultureInfo, out i))
+                return i;
+
+            return null;
+        }
+		
+		public static decimal GetDecimal(this IDictionary<String, object> dic, string name, decimal def = default(decimal), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (dic == null)
+                throw new ArgumentNullException(nameof(dic));
+
+            if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			object s;
+            if (dic.TryGetValue(name, out s))
+			{
+                return (decimal)Convert.ChangeType(s, typeof(decimal), cultureInfo);
 			}
 
             return def;
---
+++ ConvertExtensions.g.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace CoreFramework
+{
+	public static partial class ConvertExtensions
+    {
+		public static bool TryConvertToInt(this object value, out int result, int defaultValue = default(int), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is int) 
+			{
+				result = (int)value;
+				return true;
+			}
+
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			return int.TryParse(value.ToString(), System.Globalization.NumberStyles.Any, cultureInfo, out result);
+        }
+		public static bool TryConvertToBool(this object value, out bool result, bool defaultValue = default(bool))
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is bool) 
+			{
+				result = (bool)value;
+				return true;
+			}
+
+			return bool.TryParse(value.ToString(), out result);
+        }
+		public static bool TryConvertToDouble(this object value, out double result, double defaultValue = default(double), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is double) 
+			{
+				result = (double)value;
+				return true;
+			}
+
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			return double.TryParse(value.ToString(), System.Globalization.NumberStyles.Any, cultureInfo, out result);
+        }
+		public static bool TryConvertToFloat(this object value, out float result, float defaultValue = default(float), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is float) 
+			{
+				result = (float)value;
+				return true;
+			}
+
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			return float.TryParse(value.ToString(), System.Globalization.NumberStyles.Any, cultureInfo, out result);
+        }
+		public static bool TryConvertToShort(this object value, out short result, short defaultValue = default(short), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is short) 
+			{
+				result = (short)value;
+				return true;
+			}
+
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			return short.TryParse(value.ToString(), System.Globalization.NumberStyles.Any, cultureInfo, out result);
+        }
+		public static bool TryConvertToDecimal(this object value, out decimal result, decimal defaultValue = default(decimal), System.Globalization.CultureInfo cultureInfo = null)
+        {
+            if (value == null)
+			{
+				result = defaultValue;
+				return false;
+			}
+			else if (value is decimal) 
+			{
+				result = (decimal)value;
+				return true;
+			}
+
+			if (cultureInfo == null) cultureInfo = System.Globalization.CultureInfo.CurrentCulture;
+			return decimal.TryParse(value.ToString(), System.Globalization.NumberStyles.Any, cultureInfo, out result);
+        }
+	}
+}
